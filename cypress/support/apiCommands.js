0b3b7a273f5abf1d76b98c82026efe0f0245c240
MDola <madhu.dola@digital.education.gov.uk>
2022-11-24 12:36:01 +0000

    Add Salford api validation test

---
+++ cypress/support/apiCommands.js
@@ -0,0 +1,8 @@
+// API Commands
+Cypress.Commands.add('validJsonAPIresponse', (response)=>{
+  expect(response.status).to.eq(200);
+  expect(response).to.have.property('headers');
+  expect(response.headers).to.include({'content-type': 'application/json;charset=UTF-8'});
+  expect(response.body).to.not.be.null;
+  expect(response.body).to.have.property('records');
+});
