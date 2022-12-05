ca6e854934d264063c55b8ecae9dd38826336b0f
mdolaDFE <madhu.dola@digital.education.gov.uk>
2022-12-05 19:09:54 +0000

    FHG-749 family hubs search results page

---
+++ cypress/e2e/Frontend-FX/FHG-749-FX-searchResultsPage.spec.js
@@ -0,0 +1,25 @@
+describe('FHG-749-FX-searchResultsPage.spec',function(){
+    it('AC 1 ,2 search results page - family hubs + services or groups within my Local Authority area + back link',function(){
+        cy.visit('/PostcodeSearch')
+        cy.searchHubsPage()
+        cy.searchbypostcode('e1 2en')
+        cy.ServiceFilterPage();
+        cy.familyHubDetails();
+        cy.serviceDetails();
+        // back button 
+        cy.get('.govuk-back-link').click();
+        cy.searchHubsPage();
+    });
+    it('AC 3 search results page - post code link ',function(){
+        cy.visit('/PostcodeSearch')
+        cy.searchHubsPage()
+        cy.searchbypostcode('e1 2en')
+        cy.ServiceFilterPage();
+        cy.familyHubDetails();
+        cy.serviceDetails();
+        // back button 
+        cy.get('main#main-content > div:nth-of-type(1) a').contains('E1 2EN').click();
+        cy.searchHubsPage();
+    });
+
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -20,7 +20,34 @@ beforeEach(() => {
   })
   Cypress.Commands.add('ServiceFilterPage',()=>{
     cy.contains('Your local family hubs, services and activities')
+    cy.get('.app-family-hub').should('be.visible')
+    cy.contains("Below are the nearest search results for services and groups in your area. These may take place at different locations, like family hubs, children's centres or community centres.")
+    cy.get('.govuk-summary-list__row').contains('Service and groups')
+    cy.get('.moj-filter').should('be.visible')
   })
+  Cypress.Commands.add('familyHubDetails',()=>{
+    cy.get('.app-family-hub').contains('This is your nearest family hub. You can drop in, meet others and find general services and activities in your area.')
+    cy.get('.app-family-hub > .govuk-summary-list.govuk-summary-list--no-border > div:nth-of-type(1) > .govuk-summary-list__value').should('be.visible')
+    cy.get('.app-family-hub > .govuk-summary-list.govuk-summary-list--no-border > div:nth-of-type(2) > .govuk-summary-list__value').should('be.visible')
+    cy.get('.app-family-hub > .govuk-summary-list.govuk-summary-list--no-border > div:nth-of-type(3) > .govuk-summary-list__value').should('be.visible')
+    cy.get('.app-family-hub > .govuk-summary-list.govuk-summary-list--no-border > div:nth-of-type(4) > .govuk-summary-list__value').should('be.visible')
+    cy.get('.app-family-hub > .govuk-summary-list.govuk-summary-list--no-border > div:nth-of-type(5) > .govuk-summary-list__value').should('be.visible')
+    cy.get('.app-family-hub > .govuk-summary-list.govuk-summary-list--no-border > div:nth-of-type(6) > .govuk-summary-list__value').should('be.visible')
+  })
+  Cypress.Commands.add('serviceDetails',()=>{
+    cy.get('.app-family-hub').contains('This is your nearest family hub. You can drop in, meet others and find general services and activities in your area.')
+    cy.get('.govuk-grid-column-two-thirds > dl:nth-of-type(1) > div:nth-of-type(1) > .govuk-summary-list__value').should('be.visible')
+    cy.get('.govuk-grid-column-two-thirds > dl:nth-of-type(1) > div:nth-of-type(2) > .govuk-summary-list__value').should('be.visible')
+    cy.get('.govuk-grid-column-two-thirds > dl:nth-of-type(1) > div:nth-of-type(3) > .govuk-summary-list__value').should('be.visible')
+    cy.get('.govuk-grid-column-two-thirds > dl:nth-of-type(1) > div:nth-of-type(4) > .govuk-summary-list__value').should('be.visible')
+    cy.get('.govuk-grid-column-two-thirds > dl:nth-of-type(1) > div:nth-of-type(5) > .govuk-summary-list__value').should('be.visible')
+    cy.get('.govuk-grid-column-two-thirds > dl:nth-of-type(1) > div:nth-of-type(6) > .govuk-summary-list__value').should('be.visible')
+     cy.get('.govuk-grid-column-two-thirds > dl:nth-of-type(1) > div:nth-of-type(7) > .govuk-summary-list__value').should('be.visible')
+      cy.get('.govuk-grid-column-two-thirds > dl:nth-of-type(1) > div:nth-of-type(8) > .govuk-summary-list__value').should('be.visible')
+    
+    
+  })
+
   Cypress.Commands.add('searchbypostcode', (postcode) => {
     cy.get('input#postcode').clear().type(postcode);
     cy.get('.govuk-button').click();
