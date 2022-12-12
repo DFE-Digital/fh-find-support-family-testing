94ac17102125e2ad9ed8c2044e3823f4a1b85257
mdolaDFE <madhu.dola@digital.education.gov.uk>
2022-12-12 20:46:40 +0000

    FHG-938 Clear filters

---
+++ cypress/e2e/Frontend-FX/FHG-938-FX-clearFilters.spec.js
@@ -0,0 +1,22 @@
+describe('FHG-938-FX-clearFilters.spec',function(){
+    it('AC 1 - clear filters',function(){
+        cy.visit('/PostcodeSearch')
+        cy.searchHubsPage()
+        cy.searchbypostcode('M6 5UJ')
+        cy.ServiceFilterPage();
+        cy.familyHubDetails();
+        cy.serviceDetails();
+        // both filters unchecked
+        cy.get('.govuk-grid-column-two-thirds').contains('Family hub').should('exist')
+        cy.get('.govuk-grid-column-two-thirds').contains('Service and groups').should('exist')
+        // no filters tagged
+        cy.get('.moj-filter__selected').contains('20 miles').should('exist')
+        // use clear filers 
+        cy.clearFilters()
+        // both filters unchecked
+        cy.get('.govuk-grid-column-two-thirds').contains('Family hub').should('exist')
+        cy.get('.govuk-grid-column-two-thirds').contains('Service and groups').should('exist')
+         cy.get('.moj-filter__selected').contains('20 miles').should('not.exist')
+
+    })
+})
