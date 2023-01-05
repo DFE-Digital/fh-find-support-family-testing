4431e317f63368e918c8b370f74f8b57fcf9391b
mdolaDFE <madhu.dola@digital.education.gov.uk>
2023-01-05 15:40:44 +0000

    Refactor filters specs

--- cypress/e2e/Frontend-FX/FHG-952-FX-filter-catFamilySupport.spec.js
+++ cypress/e2e/Frontend-FX/FHG-952-FX-filter-catFamilySupport.spec.js
@@ -1,19 +1,19 @@
 describe('FHG-952-FX-filter-familySupport.spec -  Filter Search Result by Family support category',function(){
-    it('Family Support filter - clear filters ',function(){
+// Filter Search Result by Family support category
+      
+    let subcategories = ['Bullying and cyber bullying','Debt and welfare advice','Domestic abuse','Intensive targeted family support','Money, benefits and housing','Parenting support','Reducing parental conflict','Separating and separated parent support','Stopping smoking','Substance misuse (including alcohol and drug)','Targeted youth support','Youth justice services']
+
+       let subcatcode = ['1','2','3','4','5','6','7','8','9','10','11','12']
+       for (let i=0; i< subcatcode.length; i++){
+      
+    it(`${subcategories[i]} - Family Support filter - clear filters`,function(){
         cy.visit('/PostcodeSearch')
         cy.searchHubsPage()
         cy.searchbypostcode('e1 5jy')
-        cy.ServiceFilterPage();
-        cy.familyHubDetails();
-        cy.serviceDetails();
+        // cy.ServiceFilterPage();
+        // cy.familyHubDetails();
+        // cy.serviceDetails();
         
-      // Filter Search Result by Family support category
-      
-    //    let subcategories = ['Bullying and cyber bullying','Debt and welfare advice','Domestic abuse','Intensive targeted family support','Money, benefits and housing','Parenting support','Reducing parental conflict','Separating and separated parent support','Stopping smoking','Substance misuse (including alcohol and drug)','Targeted youth support','Youth justice services']
-    let subcategories = ['Bullying and cyber bullying','Intensive targeted family support','Money, benefits and housing','Parenting support']
-
-       let subcatcode = ['1','4','5','6']
-       for (let i=0; i< subcatcode.length; i++){
         cy.activitiesFilter('2',`${subcatcode[i]}`)
         cy.get('.moj-filter__selected').contains(`${subcategories[i]}`).should('exist')
         cy.get('.govuk-grid-column-two-thirds').contains(`${subcategories[i]}`).should('exist')
@@ -23,8 +23,7 @@ describe('FHG-952-FX-filter-familySupport.spec -  Filter Search Result by Family
         // use clear filers 
         cy.clearFilters()
 
-       }
     });
-    
+  }
 
 })
